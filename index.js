7817fa6b54a20d4c4c87ea22aa5c26d4f38b9b6d
MJU <marie.ju2012@outlook.com>
2020-03-13 04:52:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// add solution here
+
+function theBeatlesPlay(musicians, instruments) {
+  newStrings = new Array()
+  for let i = 0; i < musicians.length; i++ {
+    newStrings.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  console.log(newStrings)
+}

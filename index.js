ec866557b9aa4c7e25ed59e7811676ef584487b4
MJU <marie.ju2012@outlook.com>
2020-03-13 05:30:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,18 +2,16 @@
 // returns an array of strings containing what instruments each musician plays
 function theBeatlesPlay(musicians, instruments) {
   var newStrings = new Array()
-  console.log(musicians, instruments)
   for (let i = 0; i < musicians.length; i++) {
     newStrings.push(`${musicians[i]} plays ${instruments[i]}`)
   }
-  console.log(newStrings)
   return newStrings
 }
 
 // takes in any array of strings  (facts) and
 // returns a new array with exclamation points added to each element
 function johnLennonFacts(facts) {
-  index = facts.length
+  var index = facts.length
   while (index -- > 0) {
     facts[i] = `${facts[i]}!!!`
   }

20c578fb68ed59042f8c2c78bf6e3840b4015a7c
MJU <marie.ju2012@outlook.com>
2020-03-13 04:53:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 function theBeatlesPlay(musicians, instruments) {
   newStrings = new Array()
-  for let i = 0; i < musicians.length; i++ {
+  for (let i = 0; i < musicians.length; i++) {
     newStrings.push(`${musicians[i]} plays ${instruments[i]}`)
   }
   console.log(newStrings)

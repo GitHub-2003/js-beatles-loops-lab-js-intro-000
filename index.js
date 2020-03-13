5a8fff30f1d4189260875e8ad7b51927436ccde0
MJU <marie.ju2012@outlook.com>
2020-03-13 05:40:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,6 +23,6 @@ function iLoveTheBeatles(number) {
   var returnStr = new Array()
   do {
     returnStr.push("I love the Beatles!")
-  } while (number++ < 15)
+  } while (++number < 15)
   return returnStr
 }
